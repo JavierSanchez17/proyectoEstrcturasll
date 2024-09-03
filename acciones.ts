14195be6951ba18ad3fcfcfc6f44521269486997
Javier Sánchez <fs281203@gmail.com>
2024-09-02 23:31:27 -0600

    Crear Principal y Acción
    Como primer paso se creo el archivo principal para la ejecución del código (main.ts).
    
    Se agrego la clase acciones con las propiedades de una acción y el get() para cada una de ellas

---
+++ acciones.ts
@@ -0,0 +1,40 @@
+export class Acciones{
+    private simbolo: string; // Simbolo de la accion Ej.(Apple INC - AAPL)
+    private nombre: string; // Nombre de la empresa
+    private precioActual: number; // Precio actual de la acción
+    private historialPrecios: number[]; // Historial de precios
+    private cantidad: number; // Cantidad de acciones disponibles
+
+    constructor(simbolo: string, nombre: string, precioInicial: number, cantidadInicial: number){
+        this.simbolo = simbolo;
+        this.nombre = nombre;
+        this.precioActual = precioInicial; // Precio inicial de la acción sin modificar
+        this.historialPrecios = [precioInicial]; // El precio inicial es el primero en la lista
+        this.cantidad = cantidadInicial;
+    }
+
+    // Simbolo
+    public getSimbolo(): string{
+        return this.simbolo;
+    }
+
+    // Nombre
+    public getNombre(): string{
+        return this.nombre;
+    }
+
+    // Precio actual de la accion
+    public getPrecio(): number{
+        return this.precioActual;
+    }
+
+    // Obtener historial de precios
+    public getHistorial(): number[]{
+        return this.historialPrecios;
+    }
+
+    // obtener cantidad de acciones disponibles
+    public getCantidad(): number{
+        return this.cantidad
+    }
+}
