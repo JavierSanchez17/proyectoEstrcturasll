661a5b94ad33e4887d8b7939aa7287a5c0f91238
Javier Sánchez <fs281203@gmail.com>
2024-09-23 10:35:31 -0600

    Se agrego la prueba de emparejamiento
    El emparejamiento verifica si las compras y las ventas están vacías, si se cumple verifica si existen y si son iguales

--- proccesmerch.ts
+++ proccesmerch.ts
@@ -14,9 +14,24 @@ export class ProcessMerch{
 
     public addCompra(simbolo: string, nombre: string, precioActual: number, cantidad: number){
         this.compras.insert(simbolo, nombre, precioActual, cantidad)
+        this.emparejamiento()
     }
 
     public addVenta(simbolo: string, nombre: string, precioActual: number, cantidad: number){
         this.ventas.insert(simbolo, nombre, precioActual, cantidad)
+        this.emparejamiento()
+    }
+    
+    public emparejamiento(){
+        while (!this.compras.isEmpty() && !this.ventas.isEmpty()){
+            if (this.compras.checkMax() && this.ventas.checkMin() && this.compras.checkMax()!.getPrecio() >= this.ventas.checkMin()!.getPrecio()){
+                console.log("Es mayor la compra que venta o son iguales")
+                break;
+            }
+            else{
+                console.log("La compra es menor a la venta o no son iguales")
+                break;
+            }
+        }
     }
 }
