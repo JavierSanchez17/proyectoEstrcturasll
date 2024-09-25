68c1903ad71e6f75db675bfa77e2517f93a47ac1
Javier Sánchez <fs281203@gmail.com>
2024-09-25 15:24:50 -0600

    Eliminación de historial de precios
    Se elimino el historial de precios en las acciones ya que el historial se utilizo en el archivo proccesmerch

--- acciones.ts
+++ acciones.ts
@@ -2,14 +2,12 @@ export class Acciones{
     private simbolo: string; // Simbolo de la accion Ej.(Apple INC - AAPL)
     private nombre: string; // Nombre de la empresa
     private precioActual: number; // Precio actual de la acción
-    private historialPrecios: number[]; // Historial de precios
     private cantidad: number; // Cantidad de acciones disponibles
 
     constructor(simbolo: string, nombre: string, precioInicial: number, cantidadInicial: number){
         this.simbolo = simbolo;
         this.nombre = nombre;
         this.precioActual = precioInicial; // Precio inicial de la acción sin modificar
-        this.historialPrecios = [precioInicial]; // El precio inicial es el primero en la lista
         this.cantidad = cantidadInicial;
     }
 
@@ -31,12 +29,6 @@ export class Acciones{
     // Editar precio y agregar al historial(Actualizar)
     public modPrecio(nuevoPrecio: number): void{
         this.precioActual = nuevoPrecio
-        this.historialPrecios.push(this.precioActual)
-    }
-
-    // Obtener historial de precios
-    public getHistorial(): number[]{
-        return this.historialPrecios;
     }
 
     // obtener cantidad de acciones disponibles
