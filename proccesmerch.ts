ffd6a4f551150eef8b2c9a43fc143d56305c3acf
Javier Sánchez <fs281203@gmail.com>
2024-09-25 15:23:01 -0600

    Append function emparejamiento
    Se agrego la función emparejamiento que hace todo el proceso de emparejar las ordenes y realizar el proceso de compra/venta de las acciones siguiendo los parámetros estipulados en el enunciado del proyecto

--- proccesmerch.ts
+++ proccesmerch.ts
@@ -14,22 +14,44 @@ export class ProcessMerch{
 
     public addCompra(simbolo: string, nombre: string, precioActual: number, cantidad: number){
         this.compras.insert(simbolo, nombre, precioActual, cantidad)
-        this.emparejamiento()
+        if (this.ventas.isEmpty())
+            console.log("No hay ordenes de venta disponibles, la compra esta pendiente")
+        else
+            this.emparejamiento()
     }
 
     public addVenta(simbolo: string, nombre: string, precioActual: number, cantidad: number){
         this.ventas.insert(simbolo, nombre, precioActual, cantidad)
-        this.emparejamiento()
+        if (this.compras.isEmpty())
+            console.log("No hay ordenes de compra disponibles, la venta esta pendiente")
+        else
+            this.emparejamiento()
     }
     
     public emparejamiento(){
         while (!this.compras.isEmpty() && !this.ventas.isEmpty()){
+            let cantidadInterambiada: number = 0
             if (this.compras.checkMax() && this.ventas.checkMin() && this.compras.checkMax()!.getPrecio() >= this.ventas.checkMin()!.getPrecio()){
-                console.log("Es mayor la compra que venta o son iguales")
-                break;
+                if (this.compras.checkMax()!.getCantidad() < this.ventas.checkMin()!.getCantidad())
+                    cantidadInterambiada = this.compras.checkMax()!.getCantidad()
+                else
+                    cantidadInterambiada = this.ventas.checkMin()!.getCantidad()
+                
+
+                this.compras.checkMax()?.modCantidad(this.compras.checkMax()!.getCantidad() - cantidadInterambiada)
+                this.ventas.checkMin()?.modCantidad(this.ventas.checkMin()!.getCantidad() - cantidadInterambiada)
+                
+                this.historialTransacciones.push("Transaccion: Simbolo" + this.compras.checkMax()?.getSimbolo() + " - Nombre: " + this.compras.checkMax()?.getNombre() + " - Cantidad" + String(cantidadInterambiada) + " - Precio" + String(this.compras.checkMax()?.getPrecio()))
+
+                if (this.compras.checkMax()?.getCantidad() == 0)
+                    console.log(this.compras.checkMax()?.getNombre() + " eliminada de compras")
+                    this.compras.getMax()
+                if (this.ventas.checkMin()?.getCantidad() == 0)
+                    console.log(this.ventas.checkMin()?.getNombre() + " eliminada de ventas")
+                    this.ventas.getMin()
             }
             else{
-                console.log("La compra es menor a la venta o no son iguales")
+                console.log("El precio de compra: " + this.compras.checkMax()?.getPrecio() + " de " + this.compras.checkMax()?.getNombre() + " es menor al precio en venta: " + this.ventas.checkMin()?.getPrecio())
                 break;
             }
         }
